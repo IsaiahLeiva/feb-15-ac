d01ad253db60795bdecf0622f948d359eb66acdb
Isaiah <isaiah.leiva@gmail.com>
2022-02-15 19:35:38 -0700

    commit

--- app/Models/SnackService.js
+++ app/Models/SnackService.js
@@ -1,6 +1,6 @@
 
 export class SnackService {
-    // TODO CReate method to add a quarter.  This should target a property on the APPSTATE. and increase it. then console log it.
+    // TODO Create method to add a quarter.  This should target a property on the APPSTATE. and increase it. then console log it.
 
 
 }
